7a05fc26dd1a0e1fe01d878f6fdf9cc6158a88b8
zer0who <eagle7449@naver.com>
2022-11-06 16:44:51 +0900

    2025

--- D1/2029.py
+++ D1/2029.py
@@ -1,6 +1,4 @@
 T = int(input())
 for t in range(1, T+1):
     a, b = map(int, input().split())
-    val = 0
-    mod = 0
-    print("#{0} {1} {2}".format(t, val, mod))
+    print("#{0} {1} {2}".format(t, a//b, a%b))
