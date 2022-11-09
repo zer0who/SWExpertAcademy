348ef18b1c6005199d35a651c1c788c34ddaba0b
zer0who <eagle7449@naver.com>
2022-11-09 15:27:53 +0900

    1288

--- D2/1288.py
+++ D2/1288.py
@@ -5,8 +5,14 @@ for i in range(1, T+1):
     count = 0
     while(True):
         count += 1
+        whole_num = N
         while(True):
-            zero_to_nine[N//10] += 1
-            N = N
-    
-    print("#{0} {1}".format(i, count))
+            last_num = str(whole_num)[-1]
+            zero_to_nine[int(last_num)] += 1
+            if whole_num < 10:
+                break
+            whole_num = whole_num // 10
+        if 0 not in zero_to_nine:
+            print("#{0} {1}".format(i, N))
+            break
+        N = (N//count) * (count+1)
