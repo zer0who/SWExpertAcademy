3b3a2da7de6c02bd329e0182f7f69e10b2c6ab97
zer0who <eagle7449@naver.com>
2022-11-13 19:31:21 +0900

    2005 ing

---
+++ D2/2005.py
@@ -0,0 +1,11 @@
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    triangle = [[1]]
+    for i in range(N):
+        for j in range(len(triangle[i])):
+            triangle.append
+
+    print("#{}".format(t))
+    for row in triangle:
+        print(row)
