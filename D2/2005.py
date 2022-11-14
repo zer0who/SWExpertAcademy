77274e550bc208867ec37e1e1c8c5d658d3108b6
zer0who <eagle7449@naver.com>
2022-11-14 16:32:56 +0900

    2005

--- D2/2005.py
+++ D2/2005.py
@@ -1,11 +1,25 @@
+def factorial(num):
+    if num > 1:
+        return num * factorial(num - 1)
+    else:
+        return 1
+
+
 T = int(input())
 for t in range(1, T+1):
     N = int(input())
-    triangle = [[1]]
+    triangle = []
     for i in range(N):
-        for j in range(len(triangle[i])):
-            triangle.append
+        temp = []
+        if i == 0:
+            temp.append(1)
+        else:
+            for j in range(i+1):
+                temp.append(factorial(i) // (factorial(j)*factorial(i-j)))
+        triangle.append(temp)
 
     print("#{}".format(t))
     for row in triangle:
-        print(row)
+        for i in range(len(row)):
+            print(row[i], end=" ")
+        print("")
