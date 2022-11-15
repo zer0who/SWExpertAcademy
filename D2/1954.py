893268ff041fd9701d417da66952d76c64c727b7
zer0who <eagle7449@naver.com>
2022-11-15 16:53:53 +0900

    1954 ing

--- D2/1954.py
+++ D2/1954.py
@@ -8,34 +8,34 @@ for t in range(1, T+1):
     number = 1
 
     while True:
+        if number == N*N:
+            break
         for d in direction:
+            if number == N*N:
+                snail[row_index][col_index] = number
+                break
             while True:
+                print(row_index, col_index)
                 snail[row_index][col_index] = number
                 print(snail)
                 number += 1
                 row_index += d[0]
                 col_index += d[1]
+
                 if col_index == N:
-                    print("case1")
                     col_index -= 1
                     row_index += 1
                     break
                 elif col_index == -1:
-                    print("case2")
                     col_index += 1
                     row_index -= 1
                     break
                 elif row_index == N:
-                    print("case3")
                     row_index -= 1
                     col_index -= 1
                     break
                 elif snail[row_index][col_index] != 0:
-                    print("wall")
                     break
-
-        if number == N*N:
-            break
 
     print("#{}".format(t))
     for r in snail:
